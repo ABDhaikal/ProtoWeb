409d397c70dbd8f3c5ce97f8a0467f2d40185532
ABDhaikal <kakikuatku.gmail.com>
2025-02-26 14:26:30 +0700

    update

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -32,7 +32,7 @@ interface RotatingCardProps {
 
 export const AboutMeCard : React.FC<RotatingCardProps> = ({ content, rotate }) => {
    return (
-         <div className={`w-[100%] md:w-[45%] h-[100%] p-[1%] origin-top-left  bg-white rounded-sm shadow-[11.465076446533203px_11.465076446533203px_0px_0px_rgba(191,191,180,1.00)] border-4 border-black flex-col justify-start items-start  inline-flex `} style={{ transform: `rotate(${rotate}deg)` }}>
+         <div className={`w-[100%] md:w-[40%] h-[100%] p-[1%] origin-top-left  bg-white rounded-sm shadow-[11.465076446533203px_11.465076446533203px_0px_0px_rgba(191,191,180,1.00)] border-4 border-black flex-col justify-start items-start  inline-flex `} style={{ transform: `rotate(${rotate}deg)` }}>
             <div className=" text-[#181818] text-[27.99px] font-medium font-['Inter'] leading-10">
                {content}
             </div>
@@ -51,31 +51,31 @@ export const LineVector = () => {
                   <path
                      d="M66.9995 1C30.6662 74.8961 -17.4855 239.327 66.9995 324.129C221.999 479.71 39.9996 561.204 41.9996 679.172C43.5996 773.546 86.6662 902.38 108 955"
                      stroke="black"
-                     stroke-width="4"
+                     strokeWidth="4"
                   />
                   <path
                      d="M38 162.849C38 174.588 29.7033 183.645 20 183.645C10.2967 183.645 2 174.588 2 162.849C2 151.11 10.2967 142.054 20 142.054C29.7033 142.054 38 151.11 38 162.849Z"
                      fill="#FEDC57"
                      stroke="black"
-                     stroke-width="4"
+                     strokeWidth="4"
                   />
                   <path
                      d="M136 360.032C136 378.066 123.226 392.226 108 392.226C92.7738 392.226 80 378.066 80 360.032C80 341.999 92.7738 327.839 108 327.839C123.226 327.839 136 341.999 136 360.032Z"
                      fill="#FEDC57"
                      stroke="black"
-                     stroke-width="4"
+                     strokeWidth="4"
                   />
                   <path
                      d="M73 607.935C73 618.101 65.8226 625.882 57.5 625.882C49.1774 625.882 42 618.101 42 607.935C42 597.77 49.1774 589.989 57.5 589.989C65.8226 589.989 73 597.77 73 607.935Z"
                      fill="#FEDC57"
                      stroke="black"
-                     stroke-width="4"
+                     strokeWidth="4"
                   />
                   <path
                      d="M88 785.742C88 802.202 76.3454 815.086 62.5 815.086C48.6546 815.086 37 802.202 37 785.742C37 769.282 48.6546 756.398 62.5 756.398C76.3454 756.398 88 769.282 88 785.742Z"
                      fill="#FEDC57"
                      stroke="black"
-                     stroke-width="4"
+                     strokeWidth="4"
                   />
      
             </svg>
